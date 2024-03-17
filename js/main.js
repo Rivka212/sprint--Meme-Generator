96a28057202bdf90f6d726a84b0771dcba256392
Bashari Rivka <basrivka1@gmail.com>
2024-03-17 02:42:51 +0200

    Add onInit function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 'use strict'
+
+
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    renderMeme()
+    renderGallery()
+
+    // var img = document.querySelector('.images-gallery')
+    // img.addEventListener('click', onImgSelect)
+}
