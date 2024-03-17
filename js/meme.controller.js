1ff89f783292cb92f11c7121df9a8c54db23250b
Bashari Rivka <basrivka1@gmail.com>
2024-03-17 02:43:37 +0200

    Move onInit to main file

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -4,39 +4,21 @@ let gElCanvas
 let gCtx
 
 
-
-function onInit() {
-    gElCanvas = document.querySelector('canvas')
-    gCtx = gElCanvas.getContext('2d')
-    renderMeme()
-
-}
-
-
-
 function renderMeme() {
     const meme = getMeme()
     const { lines } = meme
     const [{ txt, size, color }] = lines
 
     const img = new Image()
-    img.src = 'imgs/1.jpg'
+    img.src = gImg.url
 
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight)
 
-        // gCtx.lineWidth = 1
-        // gCtx.strokeStyle = 'black'
         gCtx.fillStyle = color
         gCtx.font = size + 'px Arial'
 
         gCtx.fillText(txt, 50, 50)
-        // gCtx.strokeText(gMeme.lines[0].txt, 50, 50)
     }
 }
 
-
-
-// const strHTML = images.map(img => {
-//         return `<img src="${img.url}" alt="">`
-//     })
