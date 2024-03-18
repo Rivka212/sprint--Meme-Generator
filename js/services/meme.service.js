3a283bd560bbaf8e896b8c1b3feaac01982909be
Bashari Rivka <basrivka1@gmail.com>
2024-03-18 17:17:28 +0200

    Add createLine and createMemes

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,23 +1,55 @@
 'use strict'
 
+const STORAGE_KEY = 'memeDB'
 
- var gImg = { id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }
+var gImg
+var gImgs = createImgs()
+// var gLines = createLines()
 
-var gImgs = creatImgs()
+var gMemes = []
 
 var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'Add Text Here',
-            size: 30,
-            color: 'black'
-        }
-    ]
+    lines: [{ 
+        id:1,
+        txt: 'Add Text Here',
+        size: 30,
+        color: 'black' 
+        }] 
 }
 
 var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+
+
+function createMemes() {
+    gMemes = loadFromStorage(STORAGE_KEY)
+    _saveCarsToStorage()
+}
+
+
+
+function createLines() {
+    const lines = []
+    for (let i = 0; i < length.lines; i++) {
+        lines.push(createLine(
+            i + 1, 'Add Text Here', 30, 'black')
+        )
+    }
+    return lines
+}
+
+
+function createLine(id, txt, size, color) {
+    return {
+        id: 1,
+        txt: 'Add Text Here',
+        size: 30,
+        color: 'black'
+    }
+}
+
 
 
 function getMeme() {
@@ -28,15 +60,14 @@ function getMeme() {
 function setLineTxt() {
     const text = document.querySelector('[name="txt-meme"]').value
     gMeme.lines[0].txt = text
-    renderMeme()
 }
 
 
-function creatImgs() {
+function createImgs() {
     const images = []
     for (let i = 0; i < 2; i++) {
         images.push(
-            creatImg(
+            createImg(
                 i + 1, `imgs/${i + 1}.jpg`,
                 ` ['funny', 'cat'] `
             )
@@ -49,13 +80,11 @@ function creatImgs() {
 function setImg(imgId) {
     const img = gImgs.find(img => img.id === imgId)
     gImg = img
-
-    console.log(gImg);
 }
 
 
 
-function creatImg(id, url, keywords) {
+function createImg(id, url, keywords) {
     return {
         id,
         url,
@@ -63,3 +92,8 @@ function creatImg(id, url, keywords) {
     }
 }
 
+
+
+function _saveCarsToStorage() {
+    saveToStorage(STORAGE_KEY, gMemes)
+}
