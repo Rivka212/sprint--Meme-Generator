ee7db2df9907e4279f9596b3e21df822ff5b9a1e
Bashari Rivka <basrivka1@gmail.com>
2024-03-22 02:30:30 +0200

    Add on demo text location

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -4,6 +4,7 @@ let gElCanvas
 let gCtx
 let gCurrLine = false
 let gCuurLineIdx = 0
+let gFrame = 0
 
 
 
@@ -33,7 +34,7 @@ function renderMeme() {
         // onSetLineTxt()
         // if(gCuurLineIdx)
         onShowLines()
-        if (gCuurLineIdx === gMeme.selectedLineIdx) {
+        if (gFrame === gMeme.selectedLineIdx) {
             onAddFrame(txt)
         }
     }
@@ -46,19 +47,13 @@ function onShowLines() {
     console.log('hi');
     if (gCuurLineIdx === 0) return
     const { lines } = gMeme
-    // var [{ txt, size, color, location }] = lines
-    for (let i = 0; i < lines.length - 1; i++) {
-        // var currLines = lines[0].txt
-        lines.forEach(line => {
-            var { txt, size, color, location } = line
-            console.log(txt, size, color);
 
-            gCtx.fillStyle = color
-            gCtx.font = size + 'px Arial'
-            gCtx.fillText(txt, location.x, location.y)
-
-        })
-    }
+    lines.forEach(line => {
+        var { txt, size, color, location } = line
+        gCtx.fillStyle = color
+        gCtx.font = size + 'px Arial'
+        gCtx.fillText(txt, location.x, location.y)
+    })
 }
 
 
@@ -68,22 +63,44 @@ function onAddDemoText(txt, size, color) {
     var txt = lines[gCuurLineIdx].txt
     var size = lines[gCuurLineIdx].size
     var color = lines[gCuurLineIdx].color
-
+    var location = lines[gCuurLineIdx].location
+    // console.log(lines[gCuurLineIdx].location);
+    // console.log(location);
     // console.log('hitxte');
     var cuurSpace = 40
-    var space = gCuurLineIdx * cuurSpace
+    var space = gCuurLineIdx * cuurSpace + 50
     gCtx.fillStyle = color
     gCtx.font = size + 'px Arial'
-    gCtx.fillText(txt, 50 + space, 50 + space)
+    var x = space
+    var y = space
+    if (gCuurLineIdx === 0) gCtx.fillText(txt, x, y)
+
+    if (gCuurLineIdx !== 0) {
+        x += location.x
+        y += location.y
+        gCtx.fillText(txt, x, y)
+    }
+
+
+    gMeme.lines[gCuurLineIdx].location.x = x
+    gMeme.lines[gCuurLineIdx].location.y = y
+
+    // console.log( gMeme.lines[gCuurLineIdx].location);
     // gCtx.strokeText(txt, 50 + space, 50 + space)
     // renderMeme()
 }
 
+function checkOnCanvas(event) {
+
+    console.log();
+}
+
+
 
 function onAddFrame(text) {
-
-    if (!gCuurLineIdx === gMeme.selectedLineIdx) return
-
+    console.log('hi mi');
+    if (!gFrame === gMeme.selectedLineIdx) return
+    console.log('baymi');
     var cuurSpace = 40
     var space = gCuurLineIdx * cuurSpace
     const textWidth = gCtx.measureText(text).width
@@ -105,8 +122,10 @@ function onAddLocation(text) {
     const x = 50 + space
     const y = 50 + space
     const padding = 10
+    console.log(text);
 
     lines[gCuurLineIdx].location = { x: x - padding, y: y - 30, z: textWidth + 2 * padding, v: 40 }
+    console.log(lines[gCuurLineIdx].location);
 }
 
 
@@ -119,14 +138,18 @@ function onSetLineTxt() {
     console.log(text);
     setLineTxt(text)
     onAddFrame(text)
-    onAddLocation(text)
+    // onAddLocation(text)
     renderMeme()
 }
 
 
-function changeFontFamily() {
+function changeFontFamily(ev) {
     var fontSelect = document.querySelector('fontSelect').value
-    gCtx.font = fontSelect
+    // gCtx.font = fontSelect 'px Arial'
+    console.log('px fontSelect');
+    console.log(ev);
+    console.log(gMeme.lines[gCuurLineIdx].font);
+    gMeme.lines[gCuurLineIdx].font = fontSelect
 }
 
 
@@ -145,7 +168,7 @@ function onSetColor() {
 function showColor() {
     // document.getElementById('colorPicker').click()
     var newColor = document.getElementById('colorPicker').value
-    gMeme.lines[0].color = newColor
+    gMeme.lines[gCuurLineIdx].color = newColor
     renderMeme()
 }
 
@@ -155,8 +178,8 @@ function onChangeFontSize(elSize) {
     const { lines } = gMeme
     const [{ size }] = lines
 
-    if (elSize === 'increase') gMeme.lines[0].size = size + 5
-    else if (elSize === 'decrease') gMeme.lines[0].size = size - 5
+    if (elSize === 'increase') lines[gCuurLineIdx].size = size + 5
+    else if (elSize === 'decrease') lines[gCuurLineIdx].size = size - 5
     renderMeme()
 }
 
@@ -170,16 +193,18 @@ function onAddLines() {
     var line = {
         txt: 'Add Text Here',
         size: 30,
-        color: 'black'
+        color: 'black',
+        location: { x: 0, y: 0, z: 0, v: 0 },
     }
-    var { txt, size, color } = line
+    var { txt, size, color, location } = line
     lines.push(line)
     console.log(lines);
+    gFrame++
     gMeme.selectedLineIdx++
     console.log(gMeme.selectedLineIdx);
     document.querySelector('[name="txt-meme"]').value = ''
 
-    onAddDemoText(txt, size, color)
+    onAddDemoText(txt, size, color, location)
     onAddFrame(line.txt)
     // renderMeme()
 }
